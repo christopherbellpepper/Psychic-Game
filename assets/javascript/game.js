06a5445a2c552dfc3af3c68c299fa4636e5b6acc
christopher bell <christopherbell@christophers-MacBook-Air.local>
2019-02-28 15:50:19 -0500

    final push

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var guessChoices = [];
+
+document.onkeyup = function(event) {
+
+	var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+ 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+		 	
+
+		if (options.indexOf(userGuess) > -1) {
+
+			if (userGuess === computerGuess) {
+				wins++;
+				numGuesses = 9;
+				guessChoices = [];
+			}
+
+			if (userGuess != computerGuess) {
+				numGuesses --;
+				guessChoices.push(userGuess);
+			}
+
+			if (numGuesses === 0) {
+            	numGuesses = 9;
+			    losses ++;
+			    guessChoices = [];
+			}
+
+			var html = 
+			"<h1> The Psychic Game </h1>" +
+			"<p>Guess what letter I'm thinking of!</p>" +
+			"<p>Wins: " + wins + "</p>" +
+			"<p>Losses: " + losses + "</p>" +
+			"<p>Guesses Left: " + numGuesses + "</p>" +
+			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+			document.querySelector("#game").innerHTML = html;
+        }
+	};
